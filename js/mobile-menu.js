7dd0727615a459bbbd136df941fd6ef3cb89cfa9
Daryna Morhunova <darusya0501@gmail.com>
2023-01-06 00:26:58 +0100

    add tablet js and css

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -27,3 +27,25 @@
     bodyScrollLock.enableBodyScroll(document.body)
   })
 })()
+
+// tablet
+;(() => {
+  const mobileMenu = document.querySelector('.js-menu-container-tab')
+  const openMenuBtn = document.querySelector('.js-open-menu-tab')
+  const closeMenuBtn = document.querySelector('.js-close-menu-tab')
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen)
+    mobileMenu.classList.toggle('is-open')
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll'
+    bodyScrollLock[scrollLockMethod](document.body)
+  }
+
+  openMenuBtn.addEventListener('click', toggleMenu)
+  closeMenuBtn.addEventListener('click', toggleMenu)
+})()
